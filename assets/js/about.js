3a6a02a6c521a1972464bbd00bb9bd75b6c535ea
Nikita Mikheev <nikita.miheev.1999@mail.ru>
2023-01-28 15:30:45 +0300

    fix faq

--- assets/js/about.js
+++ assets/js/about.js
@@ -44,6 +44,17 @@ setInterval(nextSlide, 5000);
 /* faq */
 
 function faqToggle(event) {
+    const elemRemove = document.querySelector('.active');
     const elem = event.target;
+
+    if (elemRemove && elemRemove!=elem.parentElement.parentElement)
+    {
+        removeToggle(elemRemove);
+    }
+    
     elem.parentElement.parentElement.classList.toggle('active');
+}
+
+function removeToggle(elem) {
+    elem.classList.remove('active');
 }
