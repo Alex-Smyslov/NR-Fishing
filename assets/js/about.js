82f81926939803af2f09780c9f3962e6d46b76bd
NikitaMikheev <nikita.miheev.1999@mail.ru>
2023-02-08 14:47:23 +0300

    fix adaptive hover about page

--- assets/js/about.js
+++ assets/js/about.js
@@ -13,3 +13,20 @@ function faqToggle(event) {
 function removeToggle(elem) {
     elem.classList.remove('active');
 }
+
+function toggle_width(wrapp) { // для адаптива, ховер не работает на телефонах
+    var e = document.getElementById(wrapp);
+    var grid = document.querySelector('.content__grid');
+    var title = document.querySelector('.content__title');
+    if(window.screen.width<500 && e.style.width === "520px") {
+        e.setAttribute("style","width:240px");
+        title.style.left = '50%';
+        grid.style.display = "none";
+    }
+
+    else if(window.screen.width<500) {
+        e.setAttribute("style","width:520px");
+        grid.style.display = "grid";
+        title.style.left = '20%';
+    }
+ }
