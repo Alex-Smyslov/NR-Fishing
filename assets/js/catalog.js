8e1bc60889e4805fc14f993eaf9af03d9369108d
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-02-08 19:16:44 +0300

    galary commit

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,7 +1,7 @@
 let listArray = [
     {
         name: "Лодка надувная",
-        price: 120000,
+        price: 12000,
         id: 0,
         url: "assets/images/сatalog/item0.jpg"
     },
@@ -116,7 +116,12 @@ function reDrawing(listArray) { /* функция для отрисовки ма
         let newEl = document.createElement('div') /* создание нового элемента списка */
         newEl.className = 'list__item'; /* присваиваем имя элементу */
         newEl.id = listArray[index].id; /* присваиваем id элементу */
-        newEl.innerHTML = `<div class = "list__item-pos"><img class="list__pic" src = ${listArray[index].url}><div class= "list__item-text"><p>${listArray[index].name}</p><p>${String(listArray[index].price)}₽</p></div><div class= "list__item--btn"><button class="buttonEdit">Редактировать</button><button class="buttonDel">Удалить</button></div></div>`; /* помещаем текстовое значение для вывода - имя + дата + 2 кнопки*/
+        newEl.innerHTML = `<div class = "list__item-pos"><img class="list__pic" 
+		  src = ${listArray[index].url}><div class= "list__item-text">
+		  <p>${listArray[index].name}</p><p>${String(listArray[index].price)}₽</p>
+		  </div><div class= "list__item--btn">
+		  <button class="buttonEdit primary-btn primary-btn--green">Редактировать</button>
+		  <button class="buttonDel primary-btn primary-btn--green">Удалить</button></div></div>`; /* помещаем текстовое значение для вывода - имя + дата + 2 кнопки*/
         list.append(newEl); /* добавление элемента в DOM дерево */
     }
     console.log(listArray); // контроль объекта для защиты работы, потом удалить
